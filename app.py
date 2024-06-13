995a41d6b39b4391f760b7d4b3cea34891a6ac08
KALGonzalez <139096869+KALGonzalez@users.noreply.github.com>
2024-06-13 16:33:56 +0000

    .3

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 print("Hello World")
+import pandas as pd
+
+import pandas as pd
+
+# Cargar el archivo CSV en un DataFrame
+data_frame = pd.read_csv('.learn/assets/pokemon_data.csv')
+
+# Imprimir el DataFrame en la consola
+print(data_frame)
+
+
