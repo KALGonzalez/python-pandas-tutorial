59c600180562278a018d0d34aef87a94a37e6141
KALGonzalez <139096869+KALGonzalez@users.noreply.github.com>
2024-06-13 19:01:10 +0000

    .4

--- app.py
+++ app.py
@@ -9,4 +9,9 @@ data_frame = pd.read_csv('.learn/assets/pokemon_data.csv')
 # Imprimir el DataFrame en la consola
 print(data_frame)
 
+ages = pd.Series([23,45,7,34,6,63,36,78,54,34])
+
+print(ages)
+
+
 
